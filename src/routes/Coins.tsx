d2113bf40bfc48eda7fe8c937b0a7eaf9cc5079c
sangminlee98 <tkdals0975@naver.com>
2022-04-06 00:40:44 +0900

    [MODIFY] edit theme add apply theme bgColor & textColor

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${props => props.theme.accentColor};
+`;
 
 const Coins = () => {
   return (
-    <div>
-      Coins
-    </div>
+    <Title>코인</Title>
   );
 };
 
