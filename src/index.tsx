292f6b6cdf03daa48cc44cd79e4394a266a721aa
sangminlee98 <tkdals0975@naver.com>
2022-04-05 23:53:05 +0900

    install styled component package

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React, { StrictMode } from 'react';
 import {createRoot} from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { theme } from './theme';
 
 const rootElem = document.getElementById('root')!;
 const root = createRoot(rootElem);
 
 root.render(
   <StrictMode>
-    <App/>
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
   </StrictMode>
 )
