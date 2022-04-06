499cf0d79a1d4de94c73509fadbe8054179c2617
sangminlee98 <tkdals0975@naver.com>
2022-04-07 01:54:02 +0900

    [MODIFY] styling coin component

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -26,6 +26,27 @@ const Title = styled.h1`
   font-size: 48px;
   color: ${props => props.theme.accentColor};
 `;
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
 interface RouterState {
   name: string;
@@ -37,7 +58,7 @@ const Coin = () => {
   const [coinInfo, setCoinInfo] = useState<InfoData>();
   const [coinPrice, setCoinPrice] = useState<PriceData>();
   const location = useLocation();
-  const name = location.state ? (location.state as RouterState).name : 'Loading';
+  const state = location.state as RouterState;
 
   useEffect(() => {
     setLoading(true);
@@ -56,9 +77,42 @@ const Coin = () => {
   return (
     <Container>
       <Header>
-        <Title>{name}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? 'Loading' : coinInfo?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{coinInfo?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${coinInfo?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{coinInfo?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{coinInfo?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{coinPrice?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{coinPrice?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          
+        </>
+      )}
     </Container>
   );
 };
